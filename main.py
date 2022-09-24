8a76039b0c38b9d7baa44e12113b10bda365c2ee
Avivar101 <benjaminokezie.ob@gmail.com>
2022-09-24 13:30:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,10 @@ import requests
 from os import path
 import json
 
-login_url = 'https://www2.gogoanime.ee/login.html'
-anime_url = 'https://www2.gogoanime.ee/isekai-meikyuu-de-harem-wo-uncensored-episode-12'
-download_link = 'https://gogodownload.net/download.php?url=aHR0cHM6LyAdrefsdsdfwerFrefdsfrersfdsrfer363435349URASDGHUSRFSJGYfdsffsderFStewthsfSFtrftesdfjZG4xOS5hbmljYWNoZS5uZXQvdXNlcjEzNDIvZTk2MzM0ZTZhODJkYmNmYmViNDBjNWE1NmFhNDcyN2MvRVAuMS52MS4xNjM5NDk4NTYxLjM2MHAubXA0P3Rva2VuPTg4VkFiTVJFb2dTWncxQzFfV0Z4a1EmZXhwaXJlcz0xNjY0MDE0ODg3JmlkPTM4NDA0JnRpdGxlPSg2NDB4MzYwLWdvZ29hbmltZSlzY2hvb2wtZGF5cy0tZXBpc29kZS0xLm1wNA=='
-file_path = 'C:\\Users\\benja\\Videos\\anime'
-output_name = "Last Dungeon Boonies"
-ep_path = path.normpath(file_path + "/" + output_name + '.mp4')
 rootUrl = "https://www2.gogoanime.ee/"
+login_url = 'https://www2.gogoanime.ee/login.html'
+
+file_path = 'C:\\Users\\benja\\Videos\\anime'
 animeName = input("input anime name").lower().replace(' ', '-')
 quality = {1: "640x360", 2: "854x480", 3: "1280x720", 4: "1920x1080"}
 
@@ -25,52 +22,64 @@ password = 'kevinkevin'
 def downloadLink(link, filepath):
     obj = SmartDL(link, filepath)
     obj.start()
+    obj.get_progress_bar()
 
 
 # get download links
 def getLinks(url, qualitySelected):
+    # dictionary of anime quality their links
     links_dict = {}
+    # get link html
     r = s.get(url)
     soup = bs(r.text, 'html.parser')
-
     links = soup.find_all("div", class_="cf-download")
+    
     selected_quality = selectQuality(qualitySelected)
 
+    # get the download links from the html
     for link in links:
+        # loops through all quality links
         for a in link.find_all('a', href=True):
+            # get selected quality link
             if selected_quality == a.text.strip():
                 urls = (a['href'])
                 links_dict[a.text] = urls
-            else: print("not-found", selected_quality, "and", a.text.replace(" ", ""), "false")
-    pprint(links_dict)
     return links_dict
 
 
+# get the selected quality
 def selectQuality(qualitySelected):
     for x, y in quality.items():
         if qualitySelected == x:
-            print(y)
             return y
 
 
-# get the episode urls of the anime
+# get the episode urls and download path of the anime
 def getAnimeUrls(start, end):
-    animeUrl = []
+    animeUrl = {}
     for i in range(start, end + 1):
-        urlList = rootUrl + animeName + '-episode-' + str(i)
-        animeUrl.append(urlList)
+        output_name = animeName + "_" + str(i)
+        url_list = rootUrl + animeName + '-episode-' + str(i)
+        ep_path = path.normpath(file_path + "/" + animeName + "/" + output_name + '.mp4')
+        animeUrl[url_list] = ep_path
     return animeUrl
 
 
 # function to download the animes
 def downloadAnime(startEp, endEp, qualitySelected):
+    # get selected episodes anime urls
     urls = getAnimeUrls(startEp, endEp)
-    for epUrl in urls:
+    # loop through all urls and path
+    for epUrl, ePath in urls.items():
         downloadLinks = getLinks(epUrl, qualitySelected)
         if len(downloadLinks) == 0:
             print(f"EP {epUrl} NOT FOUND ")
             continue
-        pprint(downloadLinks)
+        # download anime
+        for link in downloadLinks.values():
+            print(f'about to download {ePath}')
+            downloadLink(link, ePath)
+            print("downloaded")
 
 
 s = requests.Session()
@@ -86,5 +95,7 @@ payload = {
 }
 p = s.post(login_url, data=payload)
 
-downloadAnime(1, 4, 2)
+downloadAnime(1, 2, 2)
+
+
 s.close()
